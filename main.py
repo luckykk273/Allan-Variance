c7e753bf37d38a0b91868e482d0e9fd9b90bbaae
luckykk273 <luckykk273@gmail.com>
2022-08-21 14:29:10 +0800

    Modify the example in main.py

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ if __name__ == '__main__':
     file_path = './imu0.feather'
 
     # Load csv into np array
-    df = feather.read_dataframe(file_path)
-    freq = 400
+    df = feather.read_dataframe(file_path)  # also, you can read in csv file with pandas
+    freq = 400  # define your data's sample rate
 
     # Transform gyro unit from [rad/s] to [deg/s]
     df.iloc[:, 1:4] = df.iloc[:, 1:4] * 180 / PI
-    gx = df.iloc[:, 1].to_numpy()
+    gyro_x = df.iloc[:, 1].to_numpy()
 
-    tau_x, allan_var_x = allan_variance(data=gx, f=freq)
+    # Pass data into function
+    # Note that the function only accept one axis;
+    tau_x, allan_var_x = allan_variance(data=gyro_x, f=freq)
     allan_dev_x = np.sqrt(allan_var_x)
     plot_result(tau_x, allan_dev_x)
